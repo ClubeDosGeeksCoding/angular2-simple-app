b5ffd441f04fc3447d5c27702f6a9d9a61113e48
JayrAlencar <jayralencarpereira@gmail.com>
2016-05-04 20:38:13 -0300

    Finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from 'angular2/core';
+
 @Component({
   selector:    'aplicativo',
   templateUrl: 'app/view.html'
 })
+
 export class AppComponent {
+	title = "Seu primeiro aplicativo Angular 2";
+	name = "Jayr";
 }
